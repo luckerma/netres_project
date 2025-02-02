0b4c44e3d258d817d1518aeff658c248b7f16498
Quarto GHA Workflow Runner <quarto-github-actions-publish@example.com>
2025-02-02 13:13:56 +0000

    Built site for gh-pages

--- course/RefreshWeight.m
+++
@@ -1,5 +0,0 @@
-function [Weights, Previous] = RefreshWeight(Weights, Previous, Source, Target, Dist)
-  if Weights(Source) + Dist < Weights(Target)
-    Weights(Target) = Weights(Source) + Dist;
-    Previous(Target) = Source; endif, endfunction
-
